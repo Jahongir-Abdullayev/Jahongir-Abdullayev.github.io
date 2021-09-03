58aa797188605727b3e69ddb1100fd08ed5f6510
jeckstar-938 <jeckstar_aj938@mail.ru>
2021-09-03 09:41:28 +0500

    Changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,78 @@
-let btn = document.querySelector(".my_works");
-let span = document.querySelector(".span");
-let link = document.querySelector(".webpages");
-let link1 = document.querySelector(".webpages1");
+// The place I saw this effect:
+// https://angle2.agency/
+
+// Red Staper made a NICE tutorial about this effect! You should check it out!
+// https://www.youtube.com/watch?v=LgiadQQM6mo&t=5s
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/4) - 2;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
 
 
-btn.addEventListener("click", function(){
-    span.classList.toggle("active");
-    link.classList.toggle("a_active");
-    link1.classList.toggle("a_active");
-})
 
 
+
+
+
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
